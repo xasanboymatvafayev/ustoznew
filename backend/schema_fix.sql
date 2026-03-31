89e6ef087cac7a6bf4d68cfb98a6513a2f1c205c
Xasanboy <xasanboymatvapayev@icloud.com>
2026-03-31 23:05:50 +0500

    Delete backend directory

--- backend/schema_fix.sql
+++
@@ -1,3 +0,0 @@
--- Add unique constraint for scores (needed for ON CONFLICT)
-ALTER TABLE scores ADD CONSTRAINT unique_user_assignment UNIQUE (user_id, assignment_id);
-ALTER TABLE scores ADD CONSTRAINT unique_user_group_date UNIQUE (user_id, group_id, lesson_date);
